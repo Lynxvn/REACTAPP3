e18560b42a59dfdf85c8ba20365aa00776e157a4
Lynxvn <englishlynx194@gmail.com>
2024-01-24 01:45:29 +1100

    button

--- src/Test/Test.js
+++ src/Test/Test.js
@@ -8,7 +8,14 @@ function Test() {
                 <div className="MAPV" >Management Analysis and Problem Solving</div>
                 <div className="BSSI" >Business Sustainability and Social Impact</div>
                 <div className="FMS" >Future Management Skills</div>
+                <div className="Buttoncontainer">
                 <button className="Button">CLASS</button>
+                </div>
+                <button className="ButtonThu">TIME</button>
+                <div className="Buttoncontainer">
+                <div className="Day">Day</div>
+                
+                </div>
             </div>
 
 
