b80e0c43b4d3cba05040cb1d909bf63fff1d515d
Lynxvn <englishlynx194@gmail.com>
2024-01-31 20:04:53 +1100

    new

--- src/Test/Test.js
+++ src/Test/Test.js
@@ -2,10 +2,13 @@ import "./Test.css";
 import Sun from './Sun.svg'
 import Hinh from './Hinh.svg'
 function Test() {
+    const Numbers = [1, 2, 3, 4];
+    const Name = "Thư";
+
     return (
         <>
             <div className="Total">
-                <caption className="TTS1">TIME TABLE SEMESTER 1</caption>
+                <caption className="TTS1">{Name} TIME TABLE SEMESTER 1</caption>
                 <div className="BIS" >Business Information Systems</div>
                 <div className="MAPV" >Management Analysis and Problem Solving</div>
                 <div className="BSSI" >Business Sustainability and Social Impact</div>
@@ -19,8 +22,10 @@ function Test() {
                     <div className="Time">Time</div>
                 </div>
 
-            </div>
+                <div className="Outline"><a href="https://www.w3schools.com/css/css_outline.asp"></a>Outline</div>
 
+            {/*   <div>{Numbers.map()}</div> */}
+            </div>
             <>
                 <div className="Solar">
                     <img src={Hinh} className="Earth" alt="Earth" />
@@ -33,3 +38,8 @@ function Test() {
     )
 }
 export default Test;
+
+// 1. ôn react css 
+// 2. how to create react component : 3 steps 
+// 4. ôn bài java script : Map, array, object, how to write function , index , 
+// 5. practice map , object is {key,value } pair , how to get key and value
